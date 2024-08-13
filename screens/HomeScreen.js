9744a82cc181a267f9008e7e5ec98d2e51ec03c5
James Godwill Arkoh <jamesgodwillarkoh@gmail.com>
2024-08-13 11:20:25 +0000

    Added tailwind,folder and navigation configuration to starter project

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
